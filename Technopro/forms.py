9dc571fd162f00fc0bfc93e9b5057ae5653bd0ec
il-piccola <il.piccola.estate@gmail.com>
2022-07-15 14:45:55 +0900

    20220715_02

--- Technopro/forms.py
+++ Technopro/forms.py
@@ -1,3 +1,18 @@
+import csv
 from django import forms
 from .settings import *
 
+class indexForm(forms.Form) :
+    def getWaypointList() :
+        ret = []
+        waypoint_path = os.path.join(WORKDIR, WAYPOINT_FILE)
+        f = open(waypoint_path, encoding='utf_8')
+        list = csv.reader(f)
+        for row in list :
+            ret.append((row[0], row[1]))
+        return ret
+
+    waypoint = forms.ChoiceField(
+        choices=getWaypointList(),
+        widget=forms.Select()
+    )
--- Technopro/views.py
+++ Technopro/views.py
@@ -7,24 +7,28 @@ from .forms import *
 
 def index(request) :
     params = {
-        'msg' : 'ボタン押下でスコアを取得します',
+        'msg' : 'ボタン押下で座標計算およびSIGNATEスコアを取得します',
+        'form' : indexForm(),
         'reload' : False,
         'progress' : 0,
         'anime' : 'progress-bar-animated'
     }
     if request.POST :
-        params['msg'] = 'スコア取得中です、お待ちください'
-        params['reload'] = True
-        params['progress'] = getProgress()
-        score = execSignate()
-        if score > 0 :
-            params['msg'] = 'スコアは' + str(score) + 'です'
-            params['reload'] = False
-            params['progress'] = 100
-            params['anime'] = 'progress-bar-striped'
+        form = indexForm(data=request.POST)
+        if form.is_valid() :
+            waypoint = form.cleaned_data['waypoint']
+            params['msg'] = '座標計算中です、お待ちください'
+            params['reload'] = True
+            params['progress'] = getProgress(waypoint)
+            score = execSignate()
+            if score > 0 :
+                params['msg'] = 'スコアは' + str(score) + 'です'
+                params['reload'] = False
+                params['progress'] = 100
+                params['anime'] = 'progress-bar-striped'
     return render(request, 'Technopro/index.html', params)
 
-def execSignate() :
+def execSignate(waypoint) :
     score = -1
     process_path = os.path.join(WORKDIR, PROCESS_FILE)
     if not os.path.exists(process_path) :
